3f0c97e23311ed58e363ffe31b7e4690466345d0
Alberto Zaranza <albertozaranza@outlook.com>
2024-02-11 10:35:19 -0300

    update pages with personal data

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
-export const GENERATE_SLUG_FROM_TITLE = true
-export const TRANSITION_API = true
+export const SITE_TITLE = "Portfolio | Alberto Zaranza";
+export const SITE_DESCRIPTION =
+  "Welcome to my portfolio! Browse through my projects and discover my talent and experience in software development. I hope you find something inspiring!";
+export const GENERATE_SLUG_FROM_TITLE = true;
+export const TRANSITION_API = true;
